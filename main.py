2e15b543150ac8b563513b2068d133b1bdcbaf31
Scar4400 <scar4400@gmail.com>
2024-09-02 21:03:05 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from data_fetcher import fetch_all_data
+from data_processor import process_data
+from model import train_and_evaluate
+import joblib
+import mlflow
+import mlflow.sklearn
+
+def main():
+    mlflow.set_
